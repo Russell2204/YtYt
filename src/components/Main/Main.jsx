07b5be3911e4811c90c099c37d79b0cdc1b076a0
hell4ire <ruslanabdjemilov@gmail.com>
2024-02-17 17:00:36 +0500

    frst

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import Studying from './studying/Studying'
+import Pluses from './pluses/Pluses'
 
 const Main = () => {
   return (
     <main className="main">
       <Studying />
+      <Pluses />
     </main>
   )
 }
